9b49ab14dede7c3f9f4e463ce8b945658acce183
Manish Pandey <137053707+astromanreal@users.noreply.github.com>
2024-01-10 08:48:20 +0530

    Update Parall.js

--- src/Components/Items/Parall.js
+++ src/Components/Items/Parall.js
@@ -5,7 +5,7 @@ export default function Parall() {
   return (
     <>
       <Parallcontainer
-        title="Space xploration()"
+        title="Space xploration"
         body="a great place to explore our universe and our place in it.."
       />
 
