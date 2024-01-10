bcd72417036d16c35fa049e6cf6f036d661b8b7c
Manish Pandey <137053707+astromanreal@users.noreply.github.com>
2024-01-10 09:20:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,6 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
     <App /> 
-     <Analytics />
     </BrowserRouter>
   </React.StrictMode>
    
